54c80e39e0377d8c2df5752d6c5d9d3f727c35c9
rslijp <renzo.slijp@softcause.nl>
2020-07-28 11:24:48 +0200

    Import export

--- reference-api-frontend/src/EditorControl.js
+++ reference-api-frontend/src/EditorControl.js
@@ -1,8 +1,10 @@
 import {post,revert} from './client';
-import {Button} from "react-bootstrap";
+import {Button, Form, InputGroup} from "react-bootstrap";
 import React from 'react';
 import {any, arrayOf, func, string} from "prop-types";
 import {getSlotsAsDto, setSlots} from "./model/JsonTemplate";
+import {faDownload, faUpload, faSave, faHistory, faWindowClose} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function EditorControl({id, cancelUrl, commitUrl, allNodes,onWorkBenchAvailable}){
     const onClose = ()=>{
@@ -20,17 +22,67 @@ function EditorControl({id, cancelUrl, commitUrl, allNodes,onWorkBenchAvailable}
         });
     };
 
+    const onDownload = ()=>{
+        const slots = getSlotsAsDto();
+        const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(slots));
+        const downloadAnchorNode = document.createElement('a');
+        downloadAnchorNode.setAttribute("href",     dataStr);
+        downloadAnchorNode.setAttribute("download", "workbench.json");
+        document.body.appendChild(downloadAnchorNode); // required for firefox
+        downloadAnchorNode.click();
+        downloadAnchorNode.remove();
+    };
+
+    const onUpload = (e)=>{
+        const file = e.target.files[0];
+        if(!file) {
+            alert('No file')
+            return;
+        }
+        const sizeInMb = file.size/1024/1024;
+        if(sizeInMb>5) {
+            alert('File to big')
+            return;
+        }
+        const reader = new FileReader();
+        reader.readAsText(file);
+        if(onWorkBenchAvailable) onWorkBenchAvailable(false);
+        reader.onload = function() {
+            console.log(reader.result);
+            const data = JSON.parse(reader.result);
+            setSlots({slots: data},allNodes);
+            if(onWorkBenchAvailable) onWorkBenchAvailable(true);
+        };
+        reader.onerror = function() {
+            alert('Failed to upload')
+        };
+    };
+
     const onRevert = ()=>{
         if(onWorkBenchAvailable) onWorkBenchAvailable(false);
         revert(document.location.hash.substring(1),data => {
+            console.log(data);
             setSlots(data,allNodes);
             if(onWorkBenchAvailable) onWorkBenchAvailable(true);
         });
     };
     return <div className={"editControls"}>
-        <Button variant="primary" onClick={onCommit}>Commit</Button>{' '}
-        <Button variant="secondary" onClick={onClose}>Close</Button>{' '}
-        <Button variant="warning" onClick={onRevert}>Revert</Button>
+        <Button variant="primary" title={"Commit"} onClick={onCommit}><FontAwesomeIcon icon={faSave}/></Button>{' '}
+        <Button variant="secondary"  title={"Close"} onClick={onClose}><FontAwesomeIcon icon={faWindowClose}/></Button>{' '}
+        <Button variant="warning"  title={"Revert"} onClick={onRevert}><FontAwesomeIcon icon={faHistory}/></Button>{' '}
+        <Button variant="outline-secondary" title={"Download"} onClick={onDownload}><FontAwesomeIcon icon={faDownload}/></Button>{' '}
+            <InputGroup size="sm" >
+                <InputGroup.Prepend variant="outline-secondary">
+                    <InputGroup.Text  variant="outline-secondary">{" "}<FontAwesomeIcon icon={faUpload}/>{" "}</InputGroup.Text>
+                </InputGroup.Prepend>
+                <Form.File
+                    id={"file-upload"}
+                    label={"upload"}
+                    custom
+                    onChange={onUpload}
+                />
+            </InputGroup>
+        {/*<Button variant="outline-secondary" title={"Upload"} onClick={onDownload}><FontAwesomeIcon icon={faUpload}/></Button>{' '}*/}
     </div>
 
 }
